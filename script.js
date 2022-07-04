7402acc99ff591ebb94434dac549acb736fae0f8
Kyle Tillson <kyletillson004@gmail.com>
2022-07-04 15:05:58 -0600

    added initial html css and javascript content

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "About me";
 document.querySelector("body").appendChild(h2);
+// buttton format
+function filClick() {
+    var x = document.getElementById("mypara")
+    x.innerHTML = "Ako ay 20 taong gulang, ang aking kaarawan ay Disyembre 23, 2001. Ako ay ipinanganak sa Colorado, at dito na ako tumira noon pa man. Ako ay palaging may interes sa teknolohiya kung ito ay pagbuo, pag-aaral, o pagkilos sa kung ano ang aking natutunan. Lumaki ako sa paglalaro ng baseball na gumugol ng halos buong buhay ko noong panahong iyon ngunit sulit ito. Gustung-gusto ko ang photography, at mayroon akong matinding hilig sa mga kotse.";
+}
+function filipClick() {
+    var y = document.getElementById("para")
+    y.innerHTML = "Ako ay orihinal na pumasok sa trade school upang maging isang automotive technician sa Wyotech, sa Laramie, Wyoming. Sa kasamaang palad, sa panahon ng aking oras doon ako ay nagkaroon ng isang cavernous malformation. Matagal akong pinalabas nito at kinailangan kong magpaopera sa utak para matanggal ito. Pagkatapos ng aking paggaling, sinubukan ko pa ring ituloy ang isang karera sa larangan ng automotive. Natapos ko ang pagkuha ng trabaho sa Autonation at nagtrabaho doon ng halos kalahating taon bago ito hindi na kaya ng aking katawan at utak. Napagpasyahan ko pagkatapos na pinakamahusay na pumili ng isang larangan na hindi nangangailangan sa akin na ilagay ang aking katawan sa patuloy na masipag na trabaho sa lahat ng oras. Nagsimula akong makakuha ng mga sertipikasyon sa pamamagitan ng Comptia, bago ako nagpasya na gusto kong maging isang software engineer."
+}
